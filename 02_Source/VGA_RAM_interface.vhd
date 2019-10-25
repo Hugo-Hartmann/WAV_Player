1002296b3143d9272ba17f3d18f293e086d182f8
Hugo-Hartmann <56967507+Hugo-Hartmann@users.noreply.github.com>
2019-10-25 13:55:16 +0200

    Changement interface VGA RAM
    Utilisation de bloc RAM au lieu de FF

--- 02_Source/VGA_RAM_interface.vhd
+++ 02_Source/VGA_RAM_interface.vhd
@@ -11,7 +11,7 @@
 -- Author     : Hugo HARTMANN
 -- Company    : ELSYS DESIGN
 -- Created    : 2019-10-24
--- Last update: 2019-10-24
+-- Last update: 2019-10-25
 -- Platform   : Notepad++
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
@@ -34,6 +34,9 @@ use IEEE.numeric_std.all;
 -- ENTITY DECLARATION
 --------------------------------------------------------------------------------
 entity VGA_RAM_interface is
+    generic(
+        G_BEHAVIOURAL   : boolean := false
+        );
     port(
     
         ------- Clock and RESET ------------------
@@ -63,66 +66,120 @@ end VGA_RAM_interface;
 architecture RTL of VGA_RAM_interface is
 
     --------------------------------------------------------------------------------
-    -- TYPES DECLARATIONS
+    -- CONSTANTS DECLARATIONS
     --------------------------------------------------------------------------------
-    type DELAY_LINE is array (0 to 1279) of std_logic_vector(7 downto 0);
+    constant C_VGA_HMAX : integer := 1280;
+
+    --------------------------------------------------------------------------------
+    -- COMPONENT DECLARATIONS
+    --------------------------------------------------------------------------------
+    component BRAM_8bit
+        port(
+            clka    : in std_logic;
+            ena     : in std_logic;
+            wea     : in std_logic_vector(0 downto 0);
+            addra   : in std_logic_vector(10 downto 0);
+            dina    : in std_logic_vector(7 downto 0);
+            douta   : out std_logic_vector(7 downto 0);
+            clkb    : in std_logic;
+            enb     : in std_logic;
+            web     : in std_logic_vector(0 downto 0);
+            addrb   : in std_logic_vector(10 downto 0);
+            dinb    : in std_logic_vector(7 downto 0);
+            doutb   : out std_logic_vector(7 downto 0)
+            );
+    end component;
 
     --------------------------------------------------------------------------------
     -- SIGNAL DECLARATIONS
     --------------------------------------------------------------------------------
-    signal ram_fifo : DELAY_LINE;
-    signal vga_buff : DELAY_LINE;
-    signal pixel    : std_logic_vector(15 downto 0);
+    signal addr_write   : unsigned(10 downto 0);
+    signal addr_bottom  : unsigned(10 downto 0);
+    signal addr_read    : unsigned(10 downto 0);
+    signal addr_portA   : std_logic_vector(10 downto 0);
+    signal addr_portB   : std_logic_vector(10 downto 0);
+    signal dout_portB   : std_logic_vector(7 downto 0);
+    signal pixel        : std_logic_vector(15 downto 0);
+    signal din_portB    : std_logic_vector(7 downto 0);
+    signal write_portA  : std_logic_vector(0 downto 0);
+    signal write_portB  : std_logic_vector(0 downto 0);
 
 --------------------------------------------------------------------------------
 -- BEGINNING OF THE CODE
 --------------------------------------------------------------------------------
 begin
 
+    ----------------------------------------------------------------
+    -- INSTANCE : U_BRAM
+    -- Description : Contains the 2048 last samples read
+    ----------------------------------------------------------------
+    BRAM : if G_BEHAVIOURAL=false generate
+        U_BRAM : BRAM_8bit port map(
+            clka    => clk,
+            ena     => '1',
+            wea     => write_portA,
+            addra   => addr_portA,
+            dina    => RAM_dout,
+          --douta   => 
+            clkb    => clk,
+            enb     => RAM_read_video,
+            web     => write_portB, -- always 0
+            addrb   => addr_portB,
+            dinb    => din_portB,
+            doutb   => dout_portB);
+    end generate;
+
     --------------------------------------------------------------------------------
-    -- SEQ PROCESS : P_FIFO
-    -- Description : Register data read as audio
+    -- COMBINATORY :
+    -- Description : BRAM port mapping
     --------------------------------------------------------------------------------
-    P_FIFO : process(clk, reset_n)
+    din_portB   <= (others => '0'); -- no intent to write on this port
+    write_portB <= (others => '0');
+    addr_portA  <= std_logic_vector(addr_write);
+    addr_portB  <= std_logic_vector(addr_read);
+    pixel       <= X"00" & (NOT dout_portB); -- extend pixels size to match RAM_v_add and invert y-axis
+    write_portA <= (0 downto 0 => RAM_read_audio);
+
+    --------------------------------------------------------------------------------
+    -- SEQ PROCESS : P_write
+    -- Description : Generate address to write in BRAM
+    --------------------------------------------------------------------------------
+    P_write : process(clk, reset_n)
     begin
         if(reset_n='0') then
-            for i in ram_fifo'range loop
-                ram_fifo(i) <= (others => '0');
-            end loop;
+            addr_write  <= (others => '0');
         elsif(rising_edge(clk)) then
             if(RAM_read_audio='1') then
-                ram_fifo(0) <= RAM_dout;
-                for i in 1 to ram_fifo'high loop
-                    ram_fifo(i) <= ram_fifo(i-1);
-                end loop;
+                addr_write  <= addr_write + 1;
             end if;
         end if;
     end process;
 
     --------------------------------------------------------------------------------
-    -- SEQ PROCESS : P_VGA_buff
-    -- Description : keep buffer unchanged for display
+    -- SEQ PROCESS : P_bottom
+    -- Description : keep bottom and address fixed while displaying
     --------------------------------------------------------------------------------
-    P_VGA_buff : process(clk, reset_n)
+    P_bottom : process(clk, reset_n)
     begin
         if(reset_n='0') then
-            for i in vga_buff'range loop
-                vga_buff(i) <= (others => '0');
-            end loop;
+            addr_bottom <= (others => '0');
         elsif(rising_edge(clk)) then
             if(VGA_new_frame='1') then
-                for i in vga_buff'range loop
-                    vga_buff(i) <= ram_fifo(i);
-                end loop;
+                addr_bottom <= addr_write - to_unsigned(C_VGA_HMAX, addr_write'length);
             end if;
         end if;
     end process;
 
     --------------------------------------------------------------------------------
     -- COMBINATORY :
-    -- Description : Read vga_buff
+    -- Description : Generate read_addr
     --------------------------------------------------------------------------------
-    pixel   <= X"00" & vga_buff(to_integer(unsigned(RAM_h_add)));
+    addr_read   <= addr_bottom + unsigned(RAM_h_add(10 downto 0));
+
+    --------------------------------------------------------------------------------
+    -- COMBINATORY :
+    -- Description : Pixel mapping
+    --------------------------------------------------------------------------------
     RGB_out <= (others => '1') when(pixel=RAM_v_add) else (others => '0');
 
 end RTL;
