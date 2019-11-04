fed21a9fab25b647eb246b6ea501e391ffee10f5
Hugo-Hartmann <56967507+Hugo-Hartmann@users.noreply.github.com>
2019-11-04 17:27:24 +0100

    FIR fix #3

--- 06_Scripts/FIR_coef.py
+++ 06_Scripts/FIR_coef.py
@@ -1,15 +1,27 @@
 import numpy as np
 import scipy.signal as fir
 
-FMIN = 0
-FMAX = 500
+FMIN = 7000
+FMAX = 11024
 
 fichier = "FIR_" + str(FMIN) + "_" + str(FMAX) + ".coe"
 
-filtre = np.array(fir.firwin(2047, FMAX/11025, pass_zero=True))
+filtre = np.array(fir.firwin(2047, [FMIN/11025, FMAX/11025], pass_zero=False))
+cmax = max(filtre)
+cmin = abs(min(filtre))
+if(cmin>cmax):
+    cmax = cmin
+coef = 1
+
+while(cmax < 16383):
+    coef = coef*2
+    cmax = cmax*2
+
+index = np.log2(coef)
+print(index+7, index)
 
 for i in range(len(filtre)):
-    filtre[i] = int(round(filtre[i]*32768*16, 0))
+    filtre[i] = int(round(filtre[i]*coef, 0))
 
 f=open(fichier, 'w')
 
